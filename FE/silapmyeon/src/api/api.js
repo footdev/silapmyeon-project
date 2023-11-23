ed8a95b16311f38492ae941c386ff28482d3ec28
Ki_mensoo <ekclstkfka5@gmail.com>
2023-11-23 10:44:24 +0900

    Update api.js

--- FE/silapmyeon/src/api/api.js
+++ FE/silapmyeon/src/api/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const jwt = {
   Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjo5LFwidXNlckVtYWlsXCI6XCJva2lwMDQyOEBnbWFpbC5jb21cIixcInJvbGVcIjpcIlJPTEVfVVNFUlwiLFwidHlwZVwiOlwiQVRLXCJ9IiwiaWF0IjoxNjk5NDA1MTI0LCJleHAiOjE3MDA2MTQ3MjR9.9Ey4DXj63_4AOd3OAC6SQ5ICmvc0xnWLifgwlMyLwJg",
 };
 
 const instance = axios.create({
